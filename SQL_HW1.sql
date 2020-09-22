627ae0ba644b65b514db633f58305f7f261c9dc8
noms06 <31324342+noms06@users.noreply.github.com>
2020-09-22 08:55:51 -0400

    Add files via upload

---
+++ SQL_HW1.sql
@@ -0,0 +1,4 @@
+select * from sakila.film;
+select * from sakila.category;
+select * from sakila.film_category;
+select sakila.film_category.film_id, sakila.film.title, sakila.film_category.category_id, sakila.category.name from ((sakila.film_category inner join sakila.category on sakila.film_category.category_id=sakila.category.category_id) inner join sakila.film on sakila.film.film_id=sakila.film_category.film_id);
